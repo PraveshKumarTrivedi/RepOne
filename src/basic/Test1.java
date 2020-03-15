c1ba5d33dcf69fe7e3327953b76b1a99f46e62b6
PraveshTrivedi <ptrivediqa@gmail.com>
2020-03-15 11:58:14 +0530

    Commnt 2 new Class Test1 is added

--- src/basic/Test1.java
+++ src/basic/Test1.java
@@ -1,11 +1,11 @@
 package basic;
 
-public class Test {
+public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Simple");
+		System.out.println("Test 1");
 
 	}
 
